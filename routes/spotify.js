cadfb91fe5b7815e4e8d6fd84dbd02141bd89ae2
juanmafrick04 <juanmartinfrick@gmail.com>
2024-12-16 16:41:23 -0300

    removed comments

--- routes/spotify.js
+++ routes/spotify.js
@@ -4,9 +4,9 @@ import { searchTracks, searchArtists, getTrackDetails } from "../controllers/spo
 const router = express.Router();
 
 // Rutas para Spotify
-router.get("/search/tracks/:query", searchTracks); // Buscar canciones
-router.get("/search/artists/:query", searchArtists); // Buscar artistas
-router.get("/tracks/:id", getTrackDetails); // Obtener detalles de una canción
+router.get("/search/tracks/:query", searchTracks); 
+router.get("/search/artists/:query", searchArtists); 
+router.get("/tracks/:id", getTrackDetails); 
 
 export default router;
 
